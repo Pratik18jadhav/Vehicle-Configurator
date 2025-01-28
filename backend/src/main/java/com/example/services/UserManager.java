679fba4f864a1409decb17a8b5d174f6f09adec2
Pratik Jadhav <jadhavpratik5654@gmail.com>
2025-01-28 19:47:40 +0530

    Pratik : User Registration done

--- backend/src/main/java/com/example/services/UserManager.java
+++ backend/src/main/java/com/example/services/UserManager.java
@@ -1,7 +1,14 @@
 package com.example.services;
 
+import java.util.Optional;
+
+import com.example.entities.User;
+
 public interface UserManager 
 {
+	public User addUser(User user);
+	public boolean validateUser(User user);
+	public Optional<User> getUserByUsername(String username);
 	
 }
 
--- backend/src/main/java/com/example/services/UserManagerImpl.java
+++ backend/src/main/java/com/example/services/UserManagerImpl.java
@@ -1,20 +1,33 @@
 package com.example.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.example.entities.User;
 import com.example.repository.UserRepository;
 
 @Service
 public class UserManagerImpl implements UserManager {
-	
+
 	@Autowired
-	private UserRepository userRepository;
-	
-	
-	
-	
-	
-	
+	private UserRepository userrepository;
+
+	public User addUser(User user) {
+		// TODO Auto-generated method stub
+		return userrepository.save(user);
+	}
+
+	public boolean validateUser(User user) {
+
+		return userrepository.validateUser(user.getUsername(), user.getPassword());
+	}
+
+	@Override
+	public Optional<User> getUserByUsername(String username) {
+
+		return userrepository.findByUsername(username);
+	}
 
 }
